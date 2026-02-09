238c4f80e79d549cee5299d4472eec06e0c0ed9c
Kasuya95 <thunva2004@gmail.com>
2026-02-09 14:54:58 +0700

    fix base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Front_ShareED(beta)/',
+  base: '/Front_ShareED-beta-/',
 })
